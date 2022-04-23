030697d97cba96b961e51f13d46b17889f848eb4
Nicholas Ramsay <nicholasramsay@hotmail.com>
2022-04-23 02:35:03 -0700

    - user can draw when they click on the box

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+
+
+let isPainting = false;
+document.body.onmousedown = () => (isPainting = true);
+document.body.onmouseup = () => (isPainting = false );
+
 function populatePage(size){
 let board = document.querySelector(".board");
 let squares = board.querySelectorAll("div");
@@ -8,9 +14,19 @@ board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 let amount= size * size;
 for(var i = 0; i < amount; i++){
 	let square = document.createElement('div')
+	square.addEventListener('mouseover',colorSquare);
+	square.addEventListener('mousedown',colorSquare);
 	square.style.backgroundColor = 'green';
 	board.insertAdjacentElement('beforeend',square);
    }
+}
+
+
+function colorSquare(e){
+	if(isPainting){
+		this.style.backgroundColor = 'black';
+		console.log("Painting");
+	}
 }
 
 populatePage(16);
@@ -23,3 +39,4 @@ function changeSize(input){
 		console.log('too many squares');	
 	}
 }
+
